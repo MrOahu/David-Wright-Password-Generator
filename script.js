ab9b618472ba738ecce5f87f74cff6a1dae1961d
David Wright <davidwright7793@gmail.com>
2024-01-26 18:08:17 -0500

    restructured files for deployment

--- script.js
+++ script.js
@@ -15,11 +15,16 @@ var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ]
 var specialArray = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+" ]
 var characterPool = []
+var finalPassword = []
 // get user input for how they want the password using prompt and confirms
 // create character pool that holds characters that user says yes to
 function generatePassword(){
 
-var passwordLength= window.prompt("How long would you like your password to be?")
+var passwordLength= window.prompt("How long would you like your password to be? Must be between 8 and 128 characters.")
+if(passwordLength < 8 || passwordLength > 128){
+  alert("Password length must be between 8 and 128 characters long.")
+  return
+}
 // confirm returns a true or false
 var confirmLowercase= window.confirm("Would you like lowercase letters in your password?")
 var confirmUppercase= window.confirm("Would you like uppercase letters in your password?")
@@ -43,9 +48,19 @@ if(confirmSpecial === true){
   characterPool = characterPool.concat(specialArray)
 }
 
-console.log()
+console.log(characterPool)
 
-  return "This is a new Password"
+for(var i = 0; i < passwordLength; i++){
+
+var index= Math.floor(Math.random() * characterPool.length) // if returns 1
+var convertedValue= characterPool[index] // converts to b
+finalPassword.push(convertedValue)// push places value at end of array
+
+console.log(finalPassword)
+}
+
+
+  return finalPassword.join("")
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
